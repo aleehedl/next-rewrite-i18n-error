f859b0d3279ca2a3402d79fc19d5dfc492c0c161
Alex Hedlund <alex.hedlund@vincit.fi>
2024-01-30 08:56:48 +0200

    With localeDetection

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -4,7 +4,7 @@ module.exports = {
     defaultNs: 'common',
     i18n: {
       defaultLocale: 'fi',
-      localeDetection: false,
+      localeDetection: true,
       locales: ['fi', 'en'],
     },
     localePath:
